d9f2be53f52b212002461344a36f3e4feaea81cb
alexkavanagh8 <alexkavanagh8@gmail.com>
2022-12-21 21:40:11 +0000

    Add functions to increment scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,6 @@ function runGame() {
     let firstGenPokemonArray = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];
     
     let answer = pickPokemonFromArray(firstGenPokemonArray);
-    
     displayAnswerDashes(answer);
 
 }
@@ -55,10 +54,31 @@ function pickPokemonFromArray(pokemonArray) {
     return answer;
 }
 
+function displayAnswerDashes(answer) {
+
+    let answerHTML = document.getElementById('answer');
+
+    for (let i = 0; i < answer.length; i++) {
+        answerHTML.innerHTML += "_ ";
+    }
+}
+
 function handleKeyboardInput(e) {
     e.preventDefault();
 
     console.log(this.innerText + " button works!");
+}
+
+function incrementFound() {
+
+    let oldScoreFound = parseInt(document.getElementById('score-found').innerText);
+    document.getElementById("score-found").innerText = ++oldScoreFound;
+}
+
+function incrementTrapped() {
+
+    let oldScoreTrapped = parseInt(document.getElementById('score-trapped').innerText);
+    document.getElementById("score-trapped").innerText = ++oldScoreTrapped;
 }
 
 function resetGame(e) {
