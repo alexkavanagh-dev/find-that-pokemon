4d74ae3f6d904178c9dd6f64ea6e845c1bdf734c
alexkavanagh8 <alexkavanagh8@gmail.com>
2022-12-21 19:59:03 +0000

    Pick answer from pokemon array then remove it from array

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,29 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     let resetButton = document.getElementById('reset-button');
-    resetButton.addEventListener("click", handleKeyboardInput)
+    resetButton.addEventListener("click", handleKeyboardInput);
 
-})
+    runGame();
+});
+
+function runGame() {
+
+    let firstGenPokemonArray = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];
+
+    let answer = pickPokemonFromArray(firstGenPokemonArray);
+
+    console.log(answer);
+}
+
+function pickPokemonFromArray(pokemonArray) {
+
+    let arrayNumber = Math.floor( Math.random() * pokemonArray.length );
+    let answer = pokemonArray[arrayNumber];    
+
+    pokemonArray.splice(arrayNumber, 1);
+
+    return answer;
+}
 
 function createInputKeyboard() {
 
