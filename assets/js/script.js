d08fdd45d9ffd3db8e78596b901c54f3ece76852
alexkavanagh8 <alexkavanagh8@gmail.com>
2023-01-05 21:57:06 +0000

    Add commenting to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 // Const for all 151 first generation pokemon
 const firstGenPokemonArray = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];
 
-// Create clone of the pokemon array so changes can be made without affecting the original
+// Create clone of array to allow changes without losing full original array
 let pokemonArrayClone = [...firstGenPokemonArray];
 
-// Global variables and common reused HTML elements
+// Global variables
 let livesLeft = 7;
 let answer;
+
+// Global common reused HTML elements
 const pikachuBalloons = document.getElementById('balloons');
 const trap = document.getElementById('trap');
 const animationScreen = document.getElementById('animation-screen');
@@ -15,11 +17,13 @@ const firstRowHTML = document.getElementById('first-row');
 const secondRowHTML = document.getElementById('second-row');
 const thirdRowHTML = document.getElementById('third-row');
 
+// Audio elements
 const clickAudio = document.getElementById('click-audio');
 const balloonPopAudio = document.getElementById('balloon-pop');
 const pikachuFallAudio = document.getElementById('pikachu-fall');
 const correctInputAudio = document.getElementById('correct-input-audio');
 const scorePing = document.getElementById('score-ping');
+
 // Wait for DOM to load and then create the input keyboard and start the main game
 document.addEventListener("DOMContentLoaded", function() {
 
@@ -61,7 +65,7 @@ function createInputKeyboard() {
         }
     } 
 
-    // Get buttons from the input keyboard and add event listeners to them 
+    // Get buttons from the input keyboard and add event listeners to them to call handleKeyboardInput
     let keyboardButtons = document.getElementsByClassName('letter');
 
     for (let button of keyboardButtons) {
@@ -69,6 +73,9 @@ function createInputKeyboard() {
     }
 }
 
+/**
+ * Empties innerHTML of the input keyboard before recreating it with createInputKeyboard()
+ */
 function resetKeyboard() {
 
     firstRowHTML.innerHTML = "";
@@ -79,8 +86,9 @@ function resetKeyboard() {
 }
 
 /**
- * Takes cloned pokemon array, generates a random number to pick an answer
- * then splices that answer from the array and returns the answer
+ * Checks if pokemonArrayClone is empty before picking a new answer.
+ * Otherwise displayWinScreen will be called and the game is over.
+ * Console logs the answer when picked to make testing easier
  */
 function pickPokemonFromArray() {
 
@@ -89,17 +97,18 @@ function pickPokemonFromArray() {
         displayWinScreen();
     } else {
 
+        // Generate random number using the length of the array left to pick an answer
+        // and then splice the answer out so it cannot be picked again
         let arrayNumber = Math.floor( Math.random() * pokemonArrayClone.length );
         answer = (pokemonArrayClone[arrayNumber]).toUpperCase();    
     
         pokemonArrayClone.splice(arrayNumber, 1);
+        console.log(answer);
     }
-
-    console.log(answer);
 }
 
 /**
- * Displays a dash for each letter in the answer on the webpage
+ * Display a dash for each letter in the answer on that answer screen
  */
 function displayAnswerDashes() {
 
@@ -113,22 +122,27 @@ function displayAnswerDashes() {
 }
 
 /**
- * Disables the letter was just clicked and calls checkInput()
+ * Takes in the letter clicked and uses checkInput() function to check if it is in the answer.
+ * Then continues the game accordingly depending on if the letter was correct or incorrect
  * @param {*} input 
  */
 function handleKeyboardInput(input) {
 
+    // Disable the selected letter and blanks it out
     input.style.color = "#28abfd";
     input.setAttribute("disabled", "");
 
     let isInputCorrect = checkInput(input);
-
+    
+    // If input was correct, plays affirming ping and checks if the full name has been guessed
     if (isInputCorrect) {
 
         correctInputAudio.play();
 
         let isFullNameGuessed = checkIfFullNameGuessed();
 
+        // If the full name has been guessed, plays an affirming ping, increments score, disables keyboard
+        // and after 1 second will call pickNextPokemon() to continue the game
         if (isFullNameGuessed) {
 
         scorePing.play();
@@ -137,13 +151,18 @@ function handleKeyboardInput(input) {
 
         setTimeout(() => {pickNextPokemon();}, 1000)
         }
-    } else {
+    }
+    // if input was incorrect, plays a balloon pop sound, decrements lives left
+    // and removes a balloon from pikachu 
+    else {
 
         balloonPopAudio.play();
         livesLeft--;
         pikachuBalloons.src = `assets/images/pikachu-balloon-${livesLeft}.webp`;
     }
 
+    // If all lives are gone, pikachu falling audio plays, trapped score is incremented, keyboard is disabled
+    // pikachu is shown trapped in the cage for 2 seconds before a new pokemon is selected
     if (livesLeft === 0) {
 
         pikachuFallAudio.play();
@@ -163,7 +182,9 @@ function handleKeyboardInput(input) {
 }
 
 /**
- * Checks if selected input is in answer and replaces dash(es) with letter(s)
+ * Checks if the letter clicked is in the answer
+ * Replaces dashes on answer screen with the letter if needed
+ * @returns boolean 'isInputCorrect' 
  * @param {*} input 
  */
 function checkInput(input) {
@@ -184,6 +205,9 @@ function checkInput(input) {
     return isInputCorrect;
 }
 
+/**
+ * Gets keyboard buttons in HTML and disables them from being clicked
+ */
 function disableInputKeyboard() {
 
     let keyboardButtons = document.getElementsByClassName('letter');
@@ -194,6 +218,10 @@ function disableInputKeyboard() {
     }
 }
 
+/**
+ * Checks if all the letters in the answer have been guessed
+ * @returns boolean isFullNameGuessed
+ */
 function checkIfFullNameGuessed() {
 
     let answerLettersHTML = document.getElementsByClassName('answer-letter');
@@ -210,6 +238,10 @@ function checkIfFullNameGuessed() {
     return isFullNameGuessed;
 }
 
+/**
+ * Calls resetKeyboard(), pickPokemonFromArray() and displayAnswerDashes()
+ * then resets livesLeft back to 7 and the pikachu balloon image back to 7 balloons
+ */
 function pickNextPokemon() {
 
     resetKeyboard();
@@ -220,7 +252,7 @@ function pickNextPokemon() {
 }
 
 /**
- * Increments pokemon found when a full answer is guessed correctly
+ * Gets the old 'pokemon found' score from HTML and increments it by one
  */
 function incrementFound() {
 
@@ -229,7 +261,7 @@ function incrementFound() {
 }
 
 /**
- * Increments 'trapped' score when user runs out of guesses
+ * Gets the old 'pikachus trapped' score from HTML and increments it by one
  */
 function incrementTrapped() {
 
@@ -238,10 +270,10 @@ function incrementTrapped() {
 }
 
 /**
- * Resets game when reset button is used
- * @param {*} e 
+ * Resets all variables, HTML elements and pokemonArrayClone back to their 
+ * original state and starts the game over
  */
-function resetGame(input) {
+function resetGame() {
 
     clickAudio.play();
     answer = "";
@@ -258,6 +290,10 @@ function resetGame(input) {
     displayAnswerDashes();
 }
 
+/**
+ * Disables keyboard input and displays the end game screen where the
+ * animation screen is
+ */
 function displayWinScreen() {
 
     let keyboardButtons = document.getElementsByClassName('letter');
